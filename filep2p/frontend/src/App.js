20549a4ecdbf613b7d09f5c49de604689452ef8c
Tirthan Prajapati <tirthanbhuvan@gmail.com>
2024-07-27 00:12:40 +0530

    Added navbar

--- filep2p/frontend/src/App.js
+++ filep2p/frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header.js';
+import Navbarr from './components/navbar.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Navbarr/>
     </div>
   );
 }
